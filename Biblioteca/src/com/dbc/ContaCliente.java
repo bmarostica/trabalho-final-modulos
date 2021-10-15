71f6d460a0bf042753e9dfd4d973a0fd573fd268
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-15 19:05:11 -0300

    Classe ContaCliente criada.

--- Biblioteca/src/com/dbc/ContaCliente.java
+++ Biblioteca/src/com/dbc/ContaCliente.java
@@ -2,6 +2,99 @@ package com.dbc;
 
 import java.util.*;
 
-public abstract class ContaCliente{
+public abstract class ContaCliente implements PlanosDeAssinatura {
+    private Integer idCliente;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Livro livro;
 
+    public ContaCliente(Integer idCliente, String nome, String telefone, String email, Livro livro, List<ContaCliente> listaDeContas) {
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.livro = livro;
+        this.listaDeContas = listaDeContas;
+    }
+
+    private List<ContaCliente> listaDeContas;
+
+    public ContaCliente() {
+        this.listaDeContas = new ArrayList<>();
+    }
+
+    public void adicionarContaCliente(ContaCliente novaConta) {
+        this.listaDeContas.add(novaConta);
+    }
+
+    public void listarContas() {
+        for (int i = 0; i < listaDeContas.size(); i++) {
+            System.out.println("id=" + i + " | " + listaDeContas.get(i));
+        }
+    }
+
+    public void editarId(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public void editarNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void editarTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void editarEmail(String email) {
+        this.email = email;
+    }
+
+    public void editarLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public void removerContaCliente(Integer idCliente) {
+        this.listaDeContas.remove(idCliente.intValue());
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
 }
--- Biblioteca/src/com/dbc/Main.java
+++ Biblioteca/src/com/dbc/Main.java
@@ -1,4 +1,11 @@
 package com.dbc;
 
 public class Main {
+    public static void main(String[] args) {
+        /*ContaCliente continha = new ContaCliente();
+        continha.setIdCliente(); = 12;
+        System.out.println(continha.idCliente);
+        continha.editarId(122);
+        System.out.println(continha.idCliente);*/
+    }
 }
