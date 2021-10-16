6510b8ffe3156d48cb453dedd3e64129e7d915ae
David <davidsallespro@gmail.com>
2021-10-15 22:35:39 -0300

    Classe Funcionario criada.

--- Biblioteca/src/com/dbc/Funcionario.java
+++ Biblioteca/src/com/dbc/Funcionario.java
@@ -1,6 +1,42 @@
 package com.dbc;
 
 public class Funcionario{
+    private Integer idFuncionario;
+    private String nomeFuncionario;
+    private String telefoneFuncionario;
+    private String emailFuncionario;
+
+    public Integer getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(Integer idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getTelefoneFuncionario() {
+        return telefoneFuncionario;
+    }
+
+    public void setTelefoneFuncionario(String telefoneFuncionario) {
+        this.telefoneFuncionario = telefoneFuncionario;
+    }
+
+    public String getEmailFuncionario() {
+        return emailFuncionario;
+    }
+
+    public void setEmailFuncionario(String emailFuncionario) {
+        this.emailFuncionario = emailFuncionario;
+    }
 
     public void criar() {
 
